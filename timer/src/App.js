466c7e802a548f8e134d80f31884555feb9ffe21
Amalia Callizaya <amaliapamelacallizayachavez@gmail.com>
2024-01-17 20:16:15 -0400

    Practica

---
+++ timer/src/App.js
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import Boton from './components/Boton';
+
+function App() {
+  return (
+    <div className="App">
+      <Boton/>
+    </div>
+  );
+}
+
+export default App;
